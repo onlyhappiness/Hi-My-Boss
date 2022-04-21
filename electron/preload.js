3ea6b9535cb54b334c4645e7405aa701d44c7c3e
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-21 10:18:11 +0900

    [ UPDATE ] electron

---
+++ electron/preload.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electrom']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
