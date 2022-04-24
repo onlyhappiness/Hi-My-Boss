16da6fed05d196d6d00fcb3c0a88458ea9c34a1f
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-24 22:10:29 +0900

    [ UPDATE ] Intro Page

--- App/renderer/pages/home.jsx
+++ App/renderer/pages/home.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+// import React from 'react';
+// import Head from 'next/head';
+// import Link from 'next/link';
 
-function Home() {
+// function Home() {
+//   return (
+//     <React.Fragment>
+//       <Head>
+//         <title>Home - Nextron (with-javascript-tailwindcss)</title>
+//       </Head>
+//       <div className='grid grid-col-1 text-2xl w-full text-center'>
+//         <img className='ml-auto mr-auto' src='/images/logo.png' />
+//         <span>⚡ Electron ⚡</span>
+//         <span>+</span>
+//         <span>Next.js</span>
+//         <span>+</span>
+//         <span>tailwindcss</span>
+//         <span>=</span>
+//         <span>💕 </span>
+//       </div>
+//       <div className='mt-1 w-full flex-wrap flex justify-center'>
+//         <Link href='/next'>
+//           <a className='btn-blue'>Go to next page</a>
+//         </Link>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// export default Home;
+
+// import '../styles/home.module.css';
+
+const Home = () => {
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-javascript-tailwindcss)</title>
-      </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
-      </div>
-    </React.Fragment>
+    <div
+      className='container '
+      // style="background-image: url('/images/intro.jpg')"
+      // style={{ backgroundImage: 'url(`/images/intro.jpg`)' }}
+    >
+      <img className=' bg-cover bg-center' src='/images/intro.jpg' />
+    </div>
   );
-}
+};
 
 export default Home;
