c2ac526579a2306bdbfb6bdda3c85bc2b50984fd
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-24 21:33:56 +0900

    [ SETUP ] Nextron - with-javascript-tailwindcss

--- App/renderer/pages/home.jsx
+++ App/renderer/pages/home.jsx
@@ -6,19 +6,25 @@ function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-javascript)</title>
+        <title>Home - Nextron (with-javascript-tailwindcss)</title>
       </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <img src="/images/logo.png" />
+      <div className='grid grid-col-1 text-2xl w-full text-center'>
+        <img className='ml-auto mr-auto' src='/images/logo.png' />
+        <span>⚡ Electron ⚡</span>
+        <span>+</span>
+        <span>Next.js</span>
+        <span>+</span>
+        <span>tailwindcss</span>
+        <span>=</span>
+        <span>💕 </span>
+      </div>
+      <div className='mt-1 w-full flex-wrap flex justify-center'>
+        <Link href='/next'>
+          <a className='btn-blue'>Go to next page</a>
+        </Link>
       </div>
     </React.Fragment>
   );
-};
+}
 
 export default Home;
--- App/renderer/pages/next.jsx
+++ App/renderer/pages/next.jsx
@@ -6,18 +6,19 @@ function Next() {
   return (
     <React.Fragment>
       <Head>
-        <title>Next - Nextron (with-javascript)</title>
+        <title>Next - Nextron (with-javascript-tailwindcss)</title>
       </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/home">
-            <a>Go to home page</a>
-          </Link>
-        </p>
+      <div className='grid grid-col-1 text-2xl w-full text-center'>
+        <img className='ml-auto mr-auto' src='/images/logo.png' />
+        <span>⚡ Nextron ⚡</span>
+      </div>
+      <div className='mt-1 w-full flex-wrap flex justify-center'>
+        <Link href='/home'>
+          <a className='btn-blue'>Go to home page</a>
+        </Link>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
 export default Next;
